ff0a77c8777ec007a22b500f8edfecf3c05f25b0
ShenghaoisYummy <hsupisces@hotmail.com>
2024-01-01 17:00:35 +0800

    banner-nav and menu components completed

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,7 +1,9 @@
+// public components style
 import '../../assets/style/resetYui.css';
 import '../../assets/style/layout.css';
 import './css/css.css';
 
+// public components 
 import '../../components/topbar/index.js';
 import '../../components/main-menu/index.js';
 import '../../components/searchbar/index.js';
@@ -9,6 +11,7 @@ import '../../components/footer/index.js';
 import '../../components/backtotop/index.js';
 
 
-
-
+// index page components
 import './components/carousel/index.js';
+import './components/banner-nav/index.js';
+import './components/menu/index.js';
