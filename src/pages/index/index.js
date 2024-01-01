d700245c1c6ddd1589eede2ec59ecfce33ad5dd4
ShenghaoisYummy <hsupisces@hotmail.com>
2024-01-01 16:19:58 +0800

    backtotop and footer completed

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,6 +5,10 @@ import './css/css.css';
 import '../../components/topbar/index.js';
 import '../../components/main-menu/index.js';
 import '../../components/searchbar/index.js';
+import '../../components/footer/index.js';
+import '../../components/backtotop/index.js';
+
+
 
 
 import './components/carousel/index.js';
