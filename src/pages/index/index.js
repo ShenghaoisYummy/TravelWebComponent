1da39c63fd06ef0d6a32f70c623a0fbd9abc3303
ShenghaoisYummy <hsupisces@hotmail.com>
2024-01-01 16:08:15 +0800

    searchbar completed

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,5 +4,7 @@ import './css/css.css';
 
 import '../../components/topbar/index.js';
 import '../../components/main-menu/index.js';
+import '../../components/searchbar/index.js';
+
 
 import './components/carousel/index.js';
